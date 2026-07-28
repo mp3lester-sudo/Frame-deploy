bd32906d2918345bea3f0e8dd49873b997132e6b
Michael Lester <mp3lester@gmail.com>
2026-07-28 00:13:00 -0700

    chore: remove stray Playwright screenshot script (fixes CI lint failure)

--- shot.js
+++
@@ -1,8 +0,0 @@
-const { chromium } = require('playwright');
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage({ viewport: { width: 480, height: 900 } });
-  await page.goto(process.argv[2], { waitUntil: 'networkidle' });
-  await page.screenshot({ path: process.argv[3], fullPage: true });
-  await browser.close();
-})().catch(e => { console.error(e); process.exit(1); });
